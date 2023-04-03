5fbb620ef5bd8dc718225eb8633f09821a1ee29b
Clay Blanton <dcblanton78@gmail.com>
2023-04-03 08:40:43 -1000

    Create files and add base code

---
+++ app.js
@@ -0,0 +1,22 @@
+const grid = document.getElementbyId('grid')
+
+
+
+
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  
+    for (let i = 0; i < size * size; i++) {
+      const gridElement = document.createElement('div')
+      gridElement.classList.add('grid-element')
+      gridElement.addEventListener('mouseover', changeColor)
+      gridElement.addEventListener('mousedown', changeColor)
+      grid.appendChild(gridElement)
+    }
+  }
+
+  window.onload = () => {
+    setupGrid(16)
+   
+  }
